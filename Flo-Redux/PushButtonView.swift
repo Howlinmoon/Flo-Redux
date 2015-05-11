9e8233664f12b2092d2823d5220e857bafa906b2
Jim Veneskey <github@strifecrafter.com>
2015-05-11 12:51:58 -0400

    Multi purpose button, plus or minus

--- Flo-Redux/PushButtonView.swift
+++ Flo-Redux/PushButtonView.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
+@IBDesignable
+
 class PushButtonView: UIButton {
-
+    @IBInspectable var fillColor: UIColor = UIColor.blueColor()
+    @IBInspectable var isAddButton: Bool = true
+    
     override func drawRect(rect: CGRect) {
-        // Drawing code
         var path = UIBezierPath(ovalInRect: rect)
-        UIColor.greenColor().setFill()
+        fillColor.setFill()
         path.fill()
+        // Drawing the plus sign (+)
+        let plusHeight: CGFloat = 3.0
+        //setting the width to 60% of either the screen
+        // width or height - which ever is less.
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        // Create the path
+        var plusPath = UIBezierPath()
+        
+        // set the line width to the height of the stroke
+        plusPath.lineWidth = plusHeight
+        
+        // move the initial point of the path
+        // to the start of the horizontal stroke
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2 - plusWidth/2 + 0.5,
+            y:bounds.height/2))
+        
+        // Add a point to the path at the end of the stroke
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2 + plusWidth/2 + 0.5,
+            y:bounds.height/2))
+        
+        if isAddButton {
+            println("Drawing a + button")
+            // Trying to draw the vertical line of the plus
+            // move the initial point of the path
+            // to the start of the vertical stroke
+            plusPath.moveToPoint(CGPoint(
+                x:bounds.width/2,
+                y:bounds.height/2 - plusWidth/2 + 0.5))
+            
+            // Add a point to the path at the end of the stroke
+            plusPath.addLineToPoint(CGPoint(
+                x:bounds.width/2,
+                y:bounds.height/2 + plusWidth/2 + 0.5))
+        } else {
+            println("Drawing a - button")
+        }
+        
+        // set the stroke color
+        UIColor.whiteColor().setStroke()
+        // Draw the stroke
+        plusPath.stroke()
     }
-
+    
 }
