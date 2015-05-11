15f97007800ea15f27c9e3cdf6723b785d21b969
Jim Veneskey <github@strifecrafter.com>
2015-05-11 12:31:58 -0400

    Creating the first button

---
+++ Flo-Redux/PushButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  PushButtonView.swift
+//  Flo-Redux
+//
+//  Created by jim Veneskey on 5/11/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class PushButtonView: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
