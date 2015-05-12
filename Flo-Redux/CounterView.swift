d2cd347071746a58d752425ff85b80edcd92c8c0
Jim Veneskey <github@strifecrafter.com>
2015-05-12 12:13:51 -0400

    Drawing the Arc - part 1

--- Flo-Redux/CounterView.swift
+++ Flo-Redux/CounterView.swift
@@ -21,6 +21,30 @@ class CounterView: UIView {
     
     override func drawRect(rect: CGRect) {
         // Drawing code
+        // 1
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        // 2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // 3
+        let arcWidth: CGFloat = 76
+        
+        // 4
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        // 5
+        var path = UIBezierPath(arcCenter: center,
+            radius: radius/2 - arcWidth/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        
+        // 6
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
     }
 
 }
