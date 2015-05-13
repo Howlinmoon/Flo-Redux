6f512474a40be1e975b0bbb989f275ac464360e5
Jim Veneskey <github@strifecrafter.com>
2015-05-13 19:45:48 -0400

    Just finished dragging another UIView on top of the yellow one - which was also just created

---
+++ Flo-Redux/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo-Redux
+//
+//  Created by Jim Veneskey on 5/13/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
